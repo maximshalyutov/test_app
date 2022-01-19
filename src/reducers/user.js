b06452daa6e3a8d7bbbf15796dbba3d206dd7d12
Maxim Shalyutov <maximshalyutov@gmail.com>
2022-01-19 21:18:13 +0700

    clean

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -8,14 +8,12 @@ export default function reducer(state = {}, action) {
   const {type, payload} = action
   switch (type) {
     case FETCH_USER_START:
-    console.log(112, state)
       return {
         ...state,
         fetching: true
       };
 
     case FETCH_USER_SUCCESS:
-    console.log(7, {state})
       return {
         ...state,
         fetching: false,
